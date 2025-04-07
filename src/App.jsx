9b3a62c2d6f17540a2d9d662efe884f94dc37b1f
Vesperal <hugo.cemeli@gmail.com>
2025-04-07 13:07:47 +0200

    Fiches artisan + Formulaire de contact connecté à MailDev via backend Express

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/categorie/:categorieId" element={<ArtisanListe />} />
           <Route path="/artisan/:id" element={<ArtisanDetails />} />
+          <Route path="/artisan/:id" element={<ArtisanDetails />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
