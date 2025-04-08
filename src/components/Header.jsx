537f51d72bbf4528f2201da35cb5febd0da23aa2
Vesperal <hugo.cemeli@gmail.com>
2025-04-08 18:19:59 +0200

    🌟 Ajout d'une favicon personnalisée (favicon-32.png) et configuration dans index.html

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
-import logo from '../assets/Logo.png'; 
+import logo from '../assets/images/Logo.png'; 
 
 function Header() {
   const [query, setQuery] = useState('');
