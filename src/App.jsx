5ab60c834a9bcdc694db62442567f3c621c2aefc
Vesperal <hugo.cemeli@gmail.com>
2025-04-08 17:50:29 +0200

    📄 Ajout des pages légales vierges + footer en deux blocs stylisé selon la charte

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ import NotFound from './pages/NotFound';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Recherche from './pages/Recherche';
+import MentionsLegales from './pages/MentionsLegales';
+import DonneesPersonnelles from './pages/DonneesPersonnelles';
+import Accessibilite from './pages/Accessibilite';
+import Cookies from './pages/Cookies';
+
 
 function App() {
   return (
@@ -18,6 +23,10 @@ function App() {
           <Route path="/artisan/:id" element={<ArtisanDetails />} />
           <Route path="/artisan/:id" element={<ArtisanDetails />} />
           <Route path='/recherche' element={<Recherche />} />
+          <Route path="/mentions-legales" element={<MentionsLegales />} />
+          <Route path="/donnees-personnelles" element={<DonneesPersonnelles />} />
+          <Route path="/accessibilite" element={<Accessibilite />} />
+          <Route path="/cookies" element={<Cookies />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
---
+++ src/pages/DonneesPersonnelles.jsx
@@ -0,0 +1,10 @@
+function MentionsLegales() {
+    return (
+      <div className="container my-5">
+        <h1>Données personnelles</h1>
+        <p>Cette page sera complétée ultérieurement.</p>
+      </div>
+    );
+  }
+  export default MentionsLegales;
+  
